a032a9851618d5d48bffb3065b4c748842bde57f
akira.takahashi <rike422@gmail.com>
2016-03-25 02:05:16 +0900

    Rename table name

--- db/migrations/20160324164643_create_mergeable.rb
+++ db/migrations/20160324164643_create_mergeable.rb
@@ -1,6 +1,6 @@
 Hanami::Model.migration do
   change do
-    create_table :mergebles do
+    create_table :mergeables do
       primary_key :id
       foreign_key :pull_id, :pulls, on_delete: :cascade, null: false
       column :status, Integer, null: false
